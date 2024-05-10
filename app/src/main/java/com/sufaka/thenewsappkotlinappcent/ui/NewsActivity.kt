e25c269ece9e8666c87f01ed9d5c322dab64012f
krstkaan <k.kaanakbulak@gmail.com>
2024-05-10 10:26:02 +0300

    Fragments Added Activities renamed

--- app/src/main/java/com/sufaka/thenewsappkotlinappcent/ui/NewsActivity.kt
+++ app/src/main/java/com/sufaka/thenewsappkotlinappcent/ui/NewsActivity.kt
@@ -1,16 +1,17 @@
-package com.sufaka.thenewsappkotlinappcent
+package com.sufaka.thenewsappkotlinappcent.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.sufaka.thenewsappkotlinappcent.R
 
-class MainActivity : AppCompatActivity() {
+class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_news)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
