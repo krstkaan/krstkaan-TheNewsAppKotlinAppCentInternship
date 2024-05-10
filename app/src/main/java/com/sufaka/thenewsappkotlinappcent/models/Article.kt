f1d7ab8f14ded89c3c5c564edca4f5575119ae86
krstkaan <k.kaanakbulak@gmail.com>
2024-05-10 14:31:56 +0300

    ArticleDatabase created. Converters created.

--- app/src/main/java/com/sufaka/thenewsappkotlinappcent/models/Article.kt
+++ app/src/main/java/com/sufaka/thenewsappkotlinappcent/models/Article.kt
@@ -1,6 +1,15 @@
 package com.sufaka.thenewsappkotlinappcent.models
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+@Entity(
+    tableName = "articles"
+)
 data class Article(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int? = null,
     val author: String,
     val content: String,
     val description: String,
@@ -9,4 +18,4 @@ data class Article(
     val title: String,
     val url: String,
     val urlToImage: String
-)
+): Serializable
