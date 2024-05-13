29e1810b74656452558a0218ee2a7bf53c0a85db
krstkaan <k.kaanakbulak@gmail.com>
2024-05-13 14:59:21 +0300

    Last Commit

--- app/src/main/java/com/sufaka/thenewsappkotlinappcent/util/Constants.kt
+++ app/src/main/java/com/sufaka/thenewsappkotlinappcent/util/Constants.kt
@@ -2,7 +2,7 @@ package com.sufaka.thenewsappkotlinappcent.util
 
 class Constants {
     companion object {
-        const val API_KEY = "d2bf8d23c8394e3eab1443d56d7ead8e"
+        const val API_KEY = "cc7e0cce3f2143c2a56e49b6a9d304f0"
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
         const val QUERY_PAGE_SIZE = 20
